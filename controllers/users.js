d70b3fad4f6919b0e0d279f9eb0844dad8bf790f
Timofey Voronov <timmycarousel@gmail.com>
2023-09-20 18:30:48 +0300

    изменили пост на патч в юзере

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const BadRequestError = require('../errors/bad-request-err');
-const ConflictError = require('../errors/conflict-err');
-const UnauthorizedError = require('../errors/unauthorized-err');
-const NotFoundError = require('../errors/not-found-err');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const BadRequestError = require("../errors/bad-request-err");
+const ConflictError = require("../errors/conflict-err");
+const UnauthorizedError = require("../errors/unauthorized-err");
+const NotFoundError = require("../errors/not-found-err");
 const {
   successRegistrationMessage,
   registrationError,
@@ -14,7 +14,7 @@ const {
   profileUpdateError,
   userAlreadyRegistered,
   logoutSuccessMessage,
-} = require('../constants/config');
+} = require("../constants/config");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -25,11 +25,13 @@ const register = (req, res, next) => {
 
   bcrypt
     .hash(password, 10)
-    .then((hash) => User.create({
-      name,
-      email,
-      password: hash,
-    }))
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+    )
     .then(() => {
       res.status(201).send({
         email,
@@ -38,18 +40,21 @@ const register = (req, res, next) => {
       });
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         next(new BadRequestError(registrationError));
-      } if (err.code === 11000) {
+      }
+      if (err.code === 11000) {
         // console.log('уже есть такой e-mail');
         next(new ConflictError(emailInUse));
-      } else { next(err); }
+      } else {
+        next(err);
+      }
     });
 };
 
 // выход из системы
 const logout = (req, res) => {
-  res.clearCookie('Authorization'); // Удаление куки с JWT
+  res.clearCookie("Authorization"); // Удаление куки с JWT
   res.status(200).send({ message: logoutSuccessMessage });
 };
 
@@ -60,7 +65,7 @@ const login = (req, res, next) => {
   let foundUser; // Объявление переменной user
 
   User.findOne({ email })
-    .select('+password')
+    .select("+password")
     .then((user) => {
       if (!user) {
         throw new UnauthorizedError(invalidLoginCredentials);
@@ -78,20 +83,20 @@ const login = (req, res, next) => {
 
           const token = jwt.sign(
             { _id: foundUser.id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+            NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
             {
-              expiresIn: '7d',
-            },
+              expiresIn: "7d",
+            }
           );
 
-          res.cookie('Authorization', `Bearer ${token}`, {
+          res.cookie("Authorization", `Bearer ${token}`, {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
-            sameSite: 'strict',
+            sameSite: "strict",
           });
 
-          // console.log('корректный пароль');
-          // console.log(`токен ${token}`);
+          console.log("корректный пароль");
+          console.log(`токен ${token}`);
 
           return res.status(200).send({ token });
         });
@@ -128,12 +133,12 @@ const getUserInfo = (req, res, next) => {
 
 // изменение данный пользователя
 const updateUser = (req, res, next) => {
-  const { name, about } = req.body;
+  const { name, email } = req.body;
 
   User.findByIdAndUpdate(
     req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
+    { name, email },
+    { new: true, runValidators: true }
   )
     .then((user) => {
       if (!user) {
@@ -142,7 +147,7 @@ const updateUser = (req, res, next) => {
       res.status(200).json(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         next(new BadRequestError(profileUpdateError));
       } else if (err.code === 11000) {
         next(new ConflictError(userAlreadyRegistered));
@@ -153,5 +158,10 @@ const updateUser = (req, res, next) => {
 };
 
 module.exports = {
-  logout, register, login, getUsers, getUserInfo, updateUser,
+  logout,
+  register,
+  login,
+  getUsers,
+  getUserInfo,
+  updateUser,
 };
